e6b905d3af9c430cd1a952c7ab70b371890146cb
Mad To AzenZio <89691212+madtoazenzio@users.noreply.github.com>
2021-11-28 16:39:15 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,7 +4,7 @@ class Translation(object):
 
     TRANSLATED_MSG = """Choose The language From here That I Want to Translate.👇"""
 
-    ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @Z_Bots\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Railway 🚄\n\n📱 Build :V0.4"""
+    ABOUT_MSG = """🤖 Bot : Google Translator\n\n👨‍🎓 Credit : @Z_Bots\n\n🎙️ Language : Python3\n\n📚 Lybrary : Pyrogram v1.2.9\n\n🛑 Server : Heroku 🚄\n\n📱 Build :V0.4"""
 
     CREDITS = """Credits Here"""
 
